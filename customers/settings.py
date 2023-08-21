74aa106c148fb4986cd4fb0d4e07de51da0b8068
Odetunde Mubarak <mudet04@gmail.com>
2023-08-21 15:24:19 +0100

    #

--- customers/settings.py
+++ customers/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ['https://localhost:3000']
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 
 ROOT_URLCONF = 'customers.urls'
 
